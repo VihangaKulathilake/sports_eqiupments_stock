b03e61221e1e8b328aaed84afe0754c2b40ab8a4
Your Name <your-email@example.com>
2025-09-16 11:33:14 +0530

    Login

--- login.php
+++ login.php
@@ -3,18 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/auth.css"> <title>Login</title>
+    <link rel="stylesheet" href="css/auth.css">
+    <title>Login</title>
 </head>
 <body class="auth-container">
     <form class="auth-form" action="includes/login.inc.php" method="post">
         <div class="logo-container">
             <img src="imgs/E11.png" alt="logo">
         </div>
+<<<<<<< Updated upstream
         <h1>Login </h1>
         <input type="text" name="uname" id="uname" placeholder="Username/Email"><br><br>
         <input type="password" name="pwd" id="pwd" placeholder="Password"><br><br>
         <input class="auth-btn" type="submit" name="submit" value="Login"><br><br>
         <p>Don't have an account?,<a href="signUp.php">Sign up</a></p>
+=======
+        <h1>Login</h1>
+        <input type="text" name="uname" id="uname" placeholder="Username / Email" required>
+        <input type="password" name="pwd" id="pwd" placeholder="Password" required>
+        <input class="auth-btn" type="submit" name="submit" value="Login">
+        <p>Don't have an account? <a href="signUp.php">Sign up</a></p>
+>>>>>>> Stashed changes
     </form>
 </body>
 </html>
